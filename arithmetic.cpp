71d25197d7593237f32825803c5d0b2ee8a3350b
Your Name <you@example.com>
2024-03-04 10:20:28 +1030

    base transfrom

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -5,18 +5,32 @@ using namespace std;
 
 
 void base2_convert(int input){
-	int base2=input;
-	//int counter
-	
-		while(base2>2){
-			if(base2 % 2 ==0){
-				cout<<1
-					<<endl;
-					base2=base2/2;
-			}else{
-				cout<<0
-					<<endl;
-			}
+	int base2=1;
+	int number=input;
+	int power=0;
+	int counter;
+	while(base2<=input){
+		base2=base2*2;
+		power++;
+	}
+base2=base2/2;
 
+		while(base2>=1){
+					
+			counter=base2-number
+
+
+
+				if(number <= base2 ==0){
+					cout<<1
+						<<endl;
+						number=number/2;
+						base2=base2/2;
+				}else{
+					cout<<0
+						<<endl;
+						base2=base2/2;
+				}
+			counter--;
 		}
 }
--- main_arithmetic.cpp
+++ main_arithmetic.cpp
@@ -1,10 +1,10 @@
-#include <iosteam>
+#include <iostream>
 //using namespace std:
 
 extern void base2_convert(int input);
 
 int main (){
-int input = 32
+int input = 59;
 base2_convert(input);
 	return 0;
 }
