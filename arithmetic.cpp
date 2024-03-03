8d01d94b7b52318369c730a6f54c98e3eb5252de
Your Name <you@example.com>
2024-03-04 09:59:52 +1030

    arithmetic testing2

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <cmath>
-
+using namespace std;
 //turns base 10 to base 2
 
 
-void base2_convert(int input);{
+void base2_convert(int input){
 	int base2=input;
 	//int counter
+	
+		while(base2>2){
+			if(base2 % 2 ==0){
+				cout<<1
+					<<endl;
+					base2=base2/2;
+			}else{
+				cout<<0
+					<<endl;
+			}
 
-	while(base2>=2){
-		if(input % 2 ==0){
-			cout<<1
-				<<endl;
-		}else{
-			cout<<0
-				<<endl;
 		}
-		base2=base2/2;
-	}
 }
