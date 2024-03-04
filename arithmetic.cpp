9adb23fb3ffda3d33b50f37aa449f890c9b26a25
Your Name <you@example.com>
2024-03-04 14:55:45 +1030

    finished arithmetic

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -2,36 +2,55 @@
 #include <cmath>
 using namespace std;
 
-
-int binright(int array[],int n){
-int newarray[n]=array[0];
+int binleft(int array[],int n){
+int newarray[n];
+newarray[0]=array[n-1];
 int total=0;
+//cout<<newarray[0]<<endl;
 	for (int i =0; i < n; i++){
 
-		newarray[i]=array[i+1];
+		newarray[i+1]=array[i];
 
-		if(arrayA[n-i-1]==1){
-		total=total+pow(2,i);
+		cout<<newarray[i]<<' ';
 
+		//if(newarray[n-i-1]==1){
+		//total=total+pow(2,i);
+
+		//}
+	}
+	for (int i=0; i < n; i++){
+				if (newarray[n-i-1]==1){
+				total=total+pow(2,i);
+				cout<<total<<endl;
+			
 		}
 	}
-	newarray[n]=array[0];
-return total;
+	cout<<endl;
+	
+return total;	
 }
 
 
 
 
 
+int binright(int array[],int n){
+int newarray[n];
+newarray[11]=array[0];
+int total=0;
+	for (int i =0; i < n-1; i++){
 
+		newarray[i]=array[i+1];
 
+	}
+	for (int i=0; i < n; i++){
 
-
-
-
-
-
-
+			if (newarray[n-i-1]==1){
+			total=total+pow(2,i);
+			}
+	}
+return total;
+}
 
 int binsub(int arrayA[],int arrayB[],int n,int n2){
 int sum1=0;
@@ -45,10 +64,9 @@ int total;
 	}
 	//cout<<sum1<<endl;
 	for (int i=0; i < n2; i++){
-				if (arrayB[n-i-1]==1){
+			if (arrayB[n-i-1]==1){
 			sum2=sum2+pow(2,i);
-			
-		}
+			}
 	}
 
 total=sum1-sum2;
@@ -68,10 +86,9 @@ int binadd(int arrayA[],int arrayB[],int n,int n2){
 	}
 	//cout<<sum1<<endl;
 	for (int i=0; i < n2; i++){
-				if (arrayB[n-i-1]==1){
-			sum2=sum2+pow(2,i);
-			
-		}
+			if (arrayB[n-i-1]==1){
+			sum2=sum2+pow(2,i);	
+			}
 	}
 
 total=sum1+sum2;
@@ -86,6 +103,6 @@ int main(){
 	int array2[2] = {1,1};
 	int n1=12;
 	int n2=2;
-cout<<binright(array1,n)<<endl;
+cout<<binright(array1,n1)<<endl;
 return 0;
 }
