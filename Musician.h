c0db7344577612ae6a05c9bfa3d79fd01ce3eae3
Your Name <you@example.com>
2024-03-26 13:43:24 +1030

    1-1done

--- Musician.h
+++ Musician.h
@@ -7,10 +7,12 @@ private:
 	string instrument_name;
 	int XPyears;
 public: 
-Musician();                  // a default constructor 
-// a constructor that takes the instrument played and the years of experience
-Musician(std::string instrument, int experience);      
-string get_instrument();    // returns the instrument played
-int get_experience();       // returns the number of years experience
+Musician();
+Musician(string intrument, int ecperience);
+string get_instrument();
+int get_experience();
+
+
 };
 #endif
+
